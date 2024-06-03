f391a59814800b454155c7b96426ec8ef1bdf047
JilanyRayhan <142115432+JilanyRayhan@users.noreply.github.com>
2024-06-03 14:13:16 +0600

    Add files via upload

---
+++ Build mad libs game.py
@@ -0,0 +1,7 @@
+color=input('your favourite color: ')
+pnoun=input('write a plural noun: ')
+celeb=input('your favourite celebrity: ')
+
+print('roses are '+color)
+print(pnoun+' are blue')
+print('i love you '+celeb)
