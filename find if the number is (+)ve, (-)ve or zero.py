6323f8695d1c3ffe0e02c03d1b7579bfab3f7317
JilanyRayhan <142115432+JilanyRayhan@users.noreply.github.com>
2024-06-10 17:43:04 +0600

    Add files via upload

---
+++ find if the number is (+)ve, (-)ve or zero.py
@@ -0,0 +1,7 @@
+num = float(input('enter the number: '))
+if num > 0:
+  print('its a positive number.')
+elif num < 0:
+  print('its a negative number.')
+else:
+  print('the number is equal to zero.')
