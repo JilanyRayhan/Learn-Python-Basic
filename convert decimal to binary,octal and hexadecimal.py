3e9c8e311b83af9a39c4774b025dc5acc68f512d
JilanyRayhan <142115432+JilanyRayhan@users.noreply.github.com>
2024-07-03 13:04:02 +0600

    Add files via upload

---
+++ convert decimal to binary,octal and hexadecimal.py
@@ -0,0 +1,4 @@
+decimal = int(input())
+print(f"the binary of {decimal} is:",bin(decimal))
+print(f"the octal of {decimal} is:",oct(decimal))
+print(f"the hexadecimal of {decimal} is:",hex(decimal))
