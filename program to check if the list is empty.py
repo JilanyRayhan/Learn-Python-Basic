f09af4b08af7c5a4bad3f4b4cf877c092797a331
JilanyRayhan <142115432+JilanyRayhan@users.noreply.github.com>
2024-08-31 21:32:54 +0600

    Add files via upload

---
+++ program to check if the list is empty.py
@@ -0,0 +1,25 @@
+  # solution 1
+  a = []
+
+  if not a:
+    print("the list is empty.")
+  else:
+    print(len(a))
+
+  # solution 2
+
+  b = [1,2,3,4,5]
+
+  if len(b) == 0:
+    print("the list is empty.")
+  else:
+    print(len(b))
+
+  # solution 3
+
+  c = []
+
+  if c == []:
+    print("the list is empty.")
+  else:
+    print(len(c))
