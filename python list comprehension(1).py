ca7b81a947f1754c2eac592ff0898ce18985cfe5
JilanyRayhan <142115432+JilanyRayhan@users.noreply.github.com>
2024-09-11 20:06:20 +0600

    Add files via upload

---
+++ python list comprehension(1).py
@@ -0,0 +1,7 @@
+name = ["superman","batman","wonder women","shazam","aquaman"]
+
+p = [(n.upper()) for n in name]
+print(p)name = ["superman","batman","wonder women","shazam","aquaman"]
+
+  p = [(n.upper()) for n in name]
+  print(p)
